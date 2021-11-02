d7b6d6dbee2edef2693fc3ace93402177429f730
insig <yoon5737@fssolution.co.kr>
2021-11-02 01:16:47 +0000

    convolution layer block

---
+++ models/layers/conv_block.py
@@ -0,0 +1,18 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+# Conv2d Block Module - yolov1 activation = ReLu
+class Conv2dBnRelu(nn.Module):
+    def __init__(self, in_channels, out_channels, kernel_size, stride, padding, dilation=1, groups=1, bias=True, padding_mode='zeros'):
+        super(Conv2dBnRelu, self).__init__() 
+        self.conv2d = nn.Conv2d(in_channels=in_channels, out_channels=out_channels, kernel_size=kernel_size, stride=stride, padding=padding, dilation=dilation,
+                                    groups=groups, bias=bias, padding_mode=padding_mode)
+        self.bn = nn.BatchNorm2d(out_channels)
+        self.activation = nn.ReLU()
+
+    def forward(self, x):
+        output = self.conv2d(x)
+        output = self.bn(output)
+        output = self.activation(output)
+        return output 
